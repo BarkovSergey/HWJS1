83eae1c204a2553dc80f101dbc6e1ccad22e7ec6
serge <barkovwork94@gmail.com>
2022-03-25 00:41:54 +0300

    HWJsOne

--- index.js
+++ index.js
@@ -1,187 +1,134 @@
 /*Сложение*/
 
 function sum(num1,num2){
-    const result = num1 + num2;
-    return result;
+    return num1 + num2;
 }
 
 const sumFunction = function (num1,num2){
-    const result = num1 + num2;
-    return result;
+    return  num1 + num2;
 }
 
-const sumArrow = (num1,num2) => {
-    const result = num1 + num2;
-    return result;
-}
+const sumArrow = (num1,num2) => num1 + num2;
+
 /*Вычитание*/
 
 function sub(num1,num2){
     const result = num1 - num2;
-    return result;
 }
 
 const subFunction = function (num1,num2){
-    const result = num1 - num2;
-    return result;
-}
+    return num1 - num2;
+   }
 
-const subArrow = (num1,num2) => {
-    const result = num1 - num2;
-    return result;
-}
+const subArrow = (num1,num2) => num1 - num2;
+
 /*Умножение*/
 
 function multi(num1,num2){
-    const result = num1 * num2;
-    return result;
+    return num1 * num2;
 }
 
 const multiFunction = function (num1,num2){
-    const result = num1 * num2;
-    return result;
+    return num1 * num2;
 }
 
-const multiArrow = (num1,num2) => {
-    const result = num1 * num2;
-    return result;
-}
+const multiArrow = (num1,num2) => num1 * num2;
+
 /*Деление*/
 
 function div(num1,num2){
-    const result = num1 / num2;
-    return result;
+    return num1 / num2;
 }
 
 const divFunction = function (num1,num2){
-    const result = num1 / num2;
-    return result;
+    return num1 / num2;
 }
 
-const divArrow = (num1,num2) => {
-    const result = num1 / num2;
-    return result;
-}
+const divArrow = (num1,num2) => num1 / num2;
 
 /*Деление с остатком*/
 
 function divRem(num1,num2){
-    const result = num1 % num2;
-    return result;
+    return num1 % num2;
 }
 
 const divRemFunction = function (num1,num2){
-    const result = num1 % num2;
-    return result;
+    return num1 % num2;
 }
 
-const divRemArrow = (num1,num2) => {
-    const result = num1 % num2;
-    return result;
-}
+const divRemArrow = (num1,num2) => num1 % num2;
 
 /*Больше*/
 
 function more(num1,num2){
-    const result = num1 > num2;
-    return result;
+    return num1 > num2;
 }
 
 const moreFunction = function (num1,num2){
-    const result = num1 > num2;
-    return result;
+    return num1 > num2;
 }
 
-const moreArrow = (num1,num2) => {
-    const result = num1 > num2;
-    return result;
-}
+const moreArrow = (num1,num2) => num1 > num2;
 
 /*Меньше*/
 
 function less(num1,num2){
-    const result = num1 < num2;
-    return result;
+    return num1 < num2;
 }
 
 const lessFunction = function (num1,num2){
-    const result = num1 < num2;
-    return result;
+    return num1 < num2;
 }
 
-const lessArrow = (num1,num2) => {
-    const result = num1 < num2;
-    return result;
-}
+const lessArrow = (num1,num2) => num1 < num2;
 
 /*Больше или равно*/
 
 function moreEq(num1,num2){
-    const result = num1 >= num2;
-    return result;
+    return num1 >= num2;
 }
 
 const moreEqFunction = function (num1,num2){
-    const result = num1 >= num2;
-    return result;
+    return num1 >= num2;
 }
 
-const moreEqArrow = (num1,num2) => {
-    const result = num1 >= num2;
-    return result;
-}
+const moreEqArrow = (num1,num2) => num1 >= num2;
 
 /*Меньше или равно*/
 
 function lessEq(num1,num2){
-    const result = num1 <= num2;
-    return result;
+    return num1 <= num2;
 }
 
 const lessEqFunction = function (num1,num2){
-    const result = num1 <= num2;
-    return result;
+    return num1 <= num2;
 }
 
-const lessEqArrow = (num1,num2) => {
-    const result = num1 <= num2;
-    return result;
-}
+const lessEqArrow = (num1,num2) => num1 <= num2;
 
 /*Равно*/
 
 function Eq(num1,num2){
-    const result = num1 === num2;
-    return result;
+    return num1 === num2;
 }
 
 const EqFunction = function (num1,num2){
-    const result = num1 === num2;
-    return result;
+    return num1 === num2;
 }
 
-const EqArrow = (num1,num2) => {
-    const result = num1 === num2;
-    return result;
-}
+const EqArrow = (num1,num2) => num1 === num2;
 
 /*Не равно*/
 
 function nEq(num1,num2){
-    const result = num1 !== num2;
-    return result;
+    return num1 !== num2;
 }
 
 const nEqFunction = function (num1,num2){
-    const result = num1 !== num2;
-    return result;
+    return num1 !== num2;
 }
 
-const nEqArrow = (num1,num2) => {
-    const result = num1 !== num2;
-    return result;
-}
-
+const nEqArrow = (num1,num2) => num1 !== num2;
 
 console.log('Сложение:')
 console.log(sum(2,2));
